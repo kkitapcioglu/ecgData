9d166e68f28e4c30ca242fb3834cf7b424251dc8
kkitapcioglu <73987474+kkitapcioglu@users.noreply.github.com>
2021-03-27 10:59:26 +0300

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ECGDATA_FINAL
+//
+//  Created by Kubilay Kitapçıoğlu on 17.05.2020.
+//  Copyright © 2020 Kubilay Kitapçıoğlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
